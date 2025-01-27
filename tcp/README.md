4d52209eafe2049a9942f066f98c6270b465c48b
nopnop2002 <hau26280@tree.odn.ne.jp>
2025-01-27 15:32:40 +0900

    added tcp project

---
+++ tcp/README.md
@@ -0,0 +1,65 @@
+# Sqlite3 TCP example
+This is a demo of using sqlite3 over TCP.   
+This project requires a WiFi connection.   
+
+# Changes from the original   
+- Newly created   
+
+# Installation
+```
+git clone https://github.com/nopnop2002/esp32-idf-sqlite3
+cd esp32-idf-sqlite3/tcp
+idf.py menuconfig
+idf.py flash monitor
+```
+
+# Configuration
+![Image](https://github.com/user-attachments/assets/9b703004-7522-4624-8ae8-373f7c0544dd)
+![Image](https://github.com/user-attachments/assets/c7fcefc3-dc7e-4ea2-ac7e-4481e3763e4f)
+
+
+# Partition size
+To support the ESP32-C series, this project uses a partition of 0xD0000 (=750K) by default.   
+If your ESP32 has more than 4M Flash, you can change ```partitions.csv```.   
+
+
+# How to use
+This project works as a TCP-Server.   
+A TCP-Client app is required.   
+There are several TCP-Clients available that run on Windows.   
+[Hercules](https://www.hw-group.com/software/hercules-setup-utility)   
+[SocketTest](https://sourceforge.net/projects/sockettest/)   
+
+- Connect tcp server   
+![Image](https://github.com/user-attachments/assets/04472473-3afa-435a-8450-1636f5a7aaf9)
+![Image](https://github.com/user-attachments/assets/11b02618-96fe-40f3-8864-cee05e6bcc99)
+
+- Create database   
+Type "CREATE TABLE test1 (id INTEGER, content);" and send.   
+![Image](https://github.com/user-attachments/assets/d7c3f6c0-8858-4f1a-9c69-c4263f9eedf8)
+
+- Get list of tables   
+Type "select name from sqlite_master where type='table';" and send.   
+![Image](https://github.com/user-attachments/assets/3f777ff0-3ecc-4bce-8ab9-f68e1666a2ed)
+
+- Get table schema (structure)   
+Type "select * from sqlite_master;" and send.   
+![Image](https://github.com/user-attachments/assets/3af26414-7480-4332-8a59-004784e2ec2a)
+
+- Delete database   
+Type "drop table test1;" and send.   
+![Image](https://github.com/user-attachments/assets/6df27a87-f668-4a7f-90c4-28899a9e1ee8)
+
+- Insert data   
+Type "INSERT INTO test1 VALUES (1, 'Hello, World from test1');" and send.   
+Type "INSERT INTO test1 VALUES (2, 'Goodbye, World from test1');" and send.   
+![Image](https://github.com/user-attachments/assets/254f2101-dbd3-4b09-a6ee-c13af3cef44b)
+![Image](https://github.com/user-attachments/assets/8f1daba1-3cf7-473c-9e6f-936792713114)
+
+- Fetch data   
+Type "select * from test1;" and send.   
+![Image](https://github.com/user-attachments/assets/849435c0-a1c3-4ad4-98da-2095771234cb)
+
+- Delete data   
+Type "delete from test1 where id = 1;" and send.   
+![Image](https://github.com/user-attachments/assets/305aab97-8e42-4f45-967d-ef664e0f4228)
---
+++ tcp/main/tcp_server.c
@@ -0,0 +1,206 @@
+/*	BSD Socket TCP Server Example
+
+	This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+	Unless required by applicable law or agreed to in writing, this
+	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+	CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "lwip/sockets.h"
+
+#include "sqlite3.h"
+#include "sqllib.h"
+
+// Connect with timeout
+esp_err_t waitForConnect (int listen_sock, int sec, int usec) {
+	fd_set master_set, working_set;
+	int max_sd = listen_sock;
+	FD_ZERO(&master_set);
+	FD_SET(listen_sock, &master_set);
+
+	struct timeval timeout;
+	timeout.tv_sec	= sec;
+	timeout.tv_usec = usec;
+	memcpy(&working_set, &master_set, sizeof(master_set));
+	ESP_LOGD(pcTaskGetName(NULL), "Waiting on connect...");
+	int err = select(max_sd + 1, &working_set, NULL, NULL, &timeout);
+	ESP_LOGI(pcTaskGetName(NULL), "select err=%d", err);
+	if (err < 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "waitForConnect select() fail");
+		return ESP_FAIL;
+	}
+	if (err == 0) {
+		ESP_LOGW(pcTaskGetName(NULL), "waitForConnect select() timed out");
+		return ESP_ERR_TIMEOUT;
+	}
+	ESP_LOGI(pcTaskGetName(NULL), "waitForConnect select() success");
+	return ESP_OK;
+}
+
+static int callback(void *data, int argc, char **argv, char **azColName) {
+	int *sock = (int *)data;
+	ESP_LOGD(pcTaskGetName(NULL), "data=%p %p %d", data, sock, *sock);
+	int i;
+	char tx_buffer[128];
+	//printf("%s: ", (const char*)data);
+	for (i = 0; i<argc; i++){
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+		sprintf(tx_buffer, "%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+		int err = send(*sock, tx_buffer, strlen(tx_buffer), 0);
+		if (err < 0) {
+			ESP_LOGE(pcTaskGetName(NULL), "Error occurred during sending: errno %d", errno);
+		}
+	}
+	printf("\n");
+	return 0;
+}
+
+int tcp_db_exec(int sock, sqlite3 *db, const char *sql, char *errmsg) {
+	ESP_LOGD(pcTaskGetName(NULL), "sock=%p %d", &sock, sock);
+	char *zErrMsg = 0;
+	printf("%s\n", sql);
+	int rc = sqlite3_exec(db, sql, callback, &sock, &zErrMsg);
+	if (rc != SQLITE_OK) {
+		printf("SQL error: %s\n", zErrMsg);
+		sprintf(errmsg, "SQL error: %s", zErrMsg);
+		sqlite3_free(zErrMsg);
+	} else {
+		printf("Operation done successfully\n");
+	}
+	return rc;
+}
+
+void tcp_server(void *pvParameters)
+{
+	char *base_path = (char *)pvParameters;
+	ESP_LOGI(pcTaskGetName(NULL), "Start TCP PORT=%d base_path=[%s]", CONFIG_TCP_PORT, base_path);
+
+	// set up address to recv
+	char addr_str[128];
+	struct sockaddr_in dest_addr;
+	dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	dest_addr.sin_family = AF_INET;
+	//dest_addr.sin_port = htons(PORT);
+	dest_addr.sin_port = htons(CONFIG_TCP_PORT);
+	int addr_family = AF_INET;
+	int ip_protocol = IPPROTO_IP;
+	inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
+
+	int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+	if (listen_sock < 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "Unable to create socket: errno %d", errno);
+		return;
+	}
+	ESP_LOGI(pcTaskGetName(NULL), "Socket created");
+
+	int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+	if (err != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "Socket unable to bind: errno %d", errno);
+		return;
+	}
+	ESP_LOGI(pcTaskGetName(NULL), "Socket bound, port %d", CONFIG_TCP_PORT);
+
+	err = listen(listen_sock, 1);
+	if (err != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "Error occurred during listen: errno %d", errno);
+		return;
+	}
+	ESP_LOGI(pcTaskGetName(NULL), "Socket listening");
+
+	while (1) {
+		// Wait for connect
+		ESP_LOGI(pcTaskGetName(NULL), "Waiting for connect");
+		err = waitForConnect (listen_sock, 5, 0);
+		if (err == ESP_ERR_TIMEOUT) continue;
+		if (err == ESP_FAIL) break;
+
+		struct sockaddr_in6 source_addr; // Large enough for both IPv4 or IPv6
+		socklen_t addr_len = sizeof(source_addr);
+		int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
+		if (sock < 0) {
+			ESP_LOGE(pcTaskGetName(NULL), "Unable to accept connection: errno %d", errno);
+			break;
+		}
+		ESP_LOGI(pcTaskGetName(NULL), "Socket accepted. sock=%d", sock);
+
+		// Open database
+		char db_name[32];
+		snprintf(db_name, sizeof(db_name)-1, "%s/example.db", base_path);
+		sqlite3 *db;
+		sqlite3_initialize();
+		if (db_open(db_name, &db)) {
+			ESP_LOGE(pcTaskGetName(NULL), "db_open fail");
+			vTaskDelete(NULL);
+		}
+
+		while (1) {
+			char rx_buffer[128];
+			int len = recv(sock, rx_buffer, sizeof(rx_buffer), 0);
+			ESP_LOGI(pcTaskGetName(NULL), "recv len=%d errno=%d", len, errno);
+
+			// Error occurred during receiving
+			if (len < 0) {
+				ESP_LOGE(pcTaskGetName(NULL), "recv failed: errno %d", errno);
+				break;
+			}
+			// Connection closed by client
+			else if (len == 0) {
+				ESP_LOGI(pcTaskGetName(NULL), "Connection closed by client");
+				break;
+			}
+			// Data received
+			else {
+				// Get the sender's ip address as string
+				if (source_addr.sin6_family == PF_INET) {
+					inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
+				} else if (source_addr.sin6_family == PF_INET6) {
+					inet6_ntoa_r(source_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+				}
+				ESP_LOGI(pcTaskGetName(NULL), "Received %d bytes from %s:", len, addr_str);
+
+				// Remove the trailing newline code
+				ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), rx_buffer, len, ESP_LOG_DEBUG);
+				char str[3];
+				str[0] = 0x0d;
+				str[1] = 0x0a;
+				str[2] = 0x0;
+				char *pos = strstr(rx_buffer, str);
+				if (pos != NULL) {
+					len = pos - &rx_buffer[0];
+					ESP_LOGD(pcTaskGetName(NULL), "pos=%p %d", pos, len);
+				}
+				rx_buffer[len] = 0;
+				ESP_LOGI(pcTaskGetName(NULL), "[%.*s]", len, rx_buffer);
+				ESP_LOGI(pcTaskGetName(NULL), "[%s]", rx_buffer);
+
+				// Execte sql 
+				char tx_buffer[128];
+				char errmsg[128];
+				int rc = tcp_db_exec(sock, db, rx_buffer, errmsg);
+				ESP_LOGI(pcTaskGetName(NULL), "rc=%d", rc);
+				if (rc != SQLITE_OK) {
+					strcpy(tx_buffer, errmsg);
+					int err = send(sock, tx_buffer, strlen(tx_buffer), 0);
+					if (err < 0) {
+						ESP_LOGE(pcTaskGetName(NULL), "Error occurred during sending: errno %d", errno);
+						break;
+					}
+				}
+			}
+		} // end while
+
+		ESP_LOGI(pcTaskGetName(NULL), "Close socket");
+		sqlite3_close(db);
+		printf("Closed database\n");
+		close(sock);
+	} // end while
+
+	// Never reach here
+	vTaskDelete(NULL);
+}
