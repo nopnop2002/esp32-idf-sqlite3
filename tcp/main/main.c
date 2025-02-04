4668a1c2c569d39abad319daaa6872d14410f4dd
nopnop2002 <>
2025-02-04 13:35:30 +0900

    Changed spaces to TAB

--- tcp/main/main.c
+++ tcp/main/main.c
@@ -37,23 +37,23 @@ static int s_retry_num = 0;
 
 static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
-		if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-				esp_wifi_connect();
-		} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-				if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
-						esp_wifi_connect();
-						s_retry_num++;
-						ESP_LOGI(TAG, "retry to connect to the AP");
-				} else {
-						xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-				}
-				ESP_LOGI(TAG,"connect to the AP fail");
-		} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-				ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-				ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-				s_retry_num = 0;
-				xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		} else {
+			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
 		}
+		ESP_LOGI(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
 }
 
 void wifi_init_sta(void)
@@ -198,8 +198,7 @@ void app_main(void)
 	}
 	ESP_ERROR_CHECK(ret);
 
-	ESP_LOGI(TAG, "Initializing SPIFFS");
-	// Maximum files that could be open at the same time is 10.
+	// Mount SPIFFS
 	char *base_path = "/spiffs";
 	ESP_ERROR_CHECK(mountSPIFFS(base_path, "storage", 2));
 
@@ -212,7 +211,7 @@ void app_main(void)
 	// Start task
 	xTaskCreate(tcp_server, "TCP_SERVER", 1024*6, (void *)base_path, 5, NULL);
 
-    while(1) {
-        vTaskDelay(1);
-    }
+	while(1) {
+		vTaskDelay(1);
+	}
 }
